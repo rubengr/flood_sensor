7ce45dba9d12b4813190778966831057525552db
Rúben Rodrigues <ruben_gr@live.com.pt>
2022-08-01 11:06:09 +0100

    Added code. Not working yet

--- fmw/flood_sensor/src/main.cpp
+++ fmw/flood_sensor/src/main.cpp
@@ -1,26 +1,29 @@
-#include <Arduino.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
+#ifdef ARDUINO
+  #include <Arduino.h>
+#endif
 
 
 void ledBlink();
 void floodSensor();
 void app();
 
-// constants won't change. Used here to set a pin number:
+const char* ssid = "ASUS";
+const char* password = "Salvador";
 const int ledPin =  D0;// the number of the LED pin
 
-// Variables will change:
 int ledState = HIGH;             // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
-
-int sensorValue = 0;  // value read from the pot
-
+int sensorValue = 0;  // value read from the sensor
 bool flood_sensor = false; //flood sensor state
+unsigned long previousLedMillis = 0, previousStateMillis = 0;        // will store last time LED was updated
+
+
+const long ledBlinkTime = 1000;           // interval at which to blink (milliseconds)
+const long readIdleTime = 1000;
 
 enum e_states{
       e_idle,
@@ -29,10 +32,28 @@ enum e_states{
       e_error
 }state;
 
+e_states current_state = e_idle;
+e_states next_state = e_idle;
 
+WiFiClient client;
+//WiFiClientSecure sclient;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
   Serial.println("Flood Sensor");
@@ -42,20 +63,28 @@ void loop() {
   // put your main code here, to run repeatedly:
   ledBlink();
   app();
+  
 }
 
 void app(){
+    unsigned long currentMillis = millis();
 
-    switch (state)
+    switch (current_state)
     {
       case e_idle:
+          if (currentMillis - previousStateMillis >= readIdleTime) {
+              previousStateMillis = currentMillis;
+              next_state = e_read_flood_sensor;
+          }
           break;
       case e_read_flood_sensor:
           floodSensor();
+          next_state = e_idle;
           break;
       default:
           break;
     }
+    current_state = next_state;
 
     
 }
@@ -65,9 +94,9 @@ void ledBlink()
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousLedMillis >= ledBlinkTime) {
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
+    previousLedMillis = currentMillis;
 
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
